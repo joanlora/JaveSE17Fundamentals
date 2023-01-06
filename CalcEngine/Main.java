a78efaa10152be6f81c063c019c1c28bdc9d7dd8
Joan Lora <joan.lora@outlook.com>
2023-01-06 14:20:54 -0500

    First Commit!

---
+++ CalcEngine/Main.java
@@ -0,0 +1,31 @@
+package CalcEngine;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        double value1 = 100.0d;
+        double value2 = 0.0d;
+        char opCode = 'd';
+        double result = 0.0d;
+
+        if (opCode == 'a') {
+            result = value1 + value2;
+        } else if (opCode == 's') {
+            result = value1 - value2;
+        } else if (opCode == 'm') {
+            result = value1 * value2;
+        } else if (opCode == 'd') {
+            if (value2 != 0){
+                result = value1 / value2;
+            }
+
+        } else {
+            System.out.println("Invalid opCode: " + opCode);
+            result = 0.0d;
+        }
+            System.out.println(result);
+
+    }
+}
---
+++ LetsGetLogical/Main.java
@@ -0,0 +1,15 @@
+package LetsGetLogical;
+
+public class Main {
+
+    public static void main(String[] args){
+        int students = 150;
+        int rooms = 1;
+
+        if (rooms != 0 && students/rooms > 30)
+            System.out.println("Crowded");
+
+        System.out.println("");
+        System.out.println("*** end of program ***");
+    }
+}
