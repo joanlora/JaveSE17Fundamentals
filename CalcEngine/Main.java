dcef8414127a9a2f71044f80a67d26e80285b7c4
Joan Lora <97771396+joanlora@users.noreply.github.com>
2023-01-18 18:56:06 -0500

    Completed Module 7!

--- CalcEngine/Main.java
+++ CalcEngine/Main.java
@@ -1,5 +1,7 @@
 package CalcEngine;
 
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -18,11 +20,55 @@ public class Main {
             for (double currentResult : results) {
                 System.out.println(currentResult);
             }
+        } else if (args.length == 1 && args[0].equals("interactive")) {
+            executeInteractively();
         } else if (args.length == 3) {
             handleCommandLine(args);
         } else {
             System.out.println("PLease provide an operation code and 2 numeric values");
         }
+    }
+
+    static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" "); // make sure split is spliing the spaces! - Joan Lora 1/18/23
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        displayResult(opCode,leftVal,rightVal,result);
+    }
+
+    private static void displayResult (char opCode, double leftVal, double rightVal, double result){
+        char symbol = symbolFromOpCode(opCode);
+        StringBuilder builder = new StringBuilder(20);
+        builder.append(leftVal);
+        builder.append(" ");
+        builder.append(symbol);
+        builder.append(" ");
+        builder.append(rightVal);
+        builder.append(" = ");
+        builder.append(result);
+        String output = builder.toString();
+        System.out.println(output);
+    }
+    private static char symbolFromOpCode(char opCode){
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        char[] symbols = {'+', '-', '*', '/'};
+        char symbol = ' ';
+        for(int index = 0; index < opCodes.length; index++){
+            if(opCode == opCodes[index]){
+                symbol = symbols[index];
+                break;
+            }
+        }
+        return symbol;
     }
 
     private static void handleCommandLine(String[] args) {
@@ -55,5 +101,24 @@ public class Main {
         }
 
         return result;
+    }
+
+    static char opCodeFromString(String operationName) {
+        char opCode = operationName.charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {"zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"};
+
+        double value = 0d;
+        for (int index = 0; index < numberWords.length; index++) {
+            if (word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
     }
 }
