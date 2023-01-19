c1079e5025e6c8cfa629c3716d9a7ab4d85581a9
Joan Lora <97771396+joanlora@users.noreply.github.com>
2023-01-18 19:33:23 -0500

    Module 9 completed

--- CalcEngine/Main.java
+++ CalcEngine/Main.java
@@ -23,6 +23,7 @@ public class Main {
 
             System.out.println("\nUsing the Math Equation Class and Array:");
             performCalculations();
+
         } else if (args.length == 1 && args[0].equals("interactive")) {
             executeInteractively();
         } else if (args.length == 3) {
@@ -34,25 +35,25 @@ public class Main {
 
     static void performCalculations() {
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+        equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
         for (MathEquation equation : equations) {
             equation.execute();
-            System.out.println("result = " + equation.result);
+            System.out.println("result = " + equation.getResult());
         }
     }
 
-    private static MathEquation create(double leftVal, double rightVal, char opCode) {
+    /* private static MathEquation create(double leftVal, double rightVal, char opCode) {
         MathEquation equation = new MathEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpCode(opCode);
         return equation;
 
-    }
+    } */
 
     static void executeInteractively() {
         System.out.println("Enter an operation and two numbers");
